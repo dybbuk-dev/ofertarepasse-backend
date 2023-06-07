d64313e86c7a2d9c53125b5109588f8bc1ac1b6d
dybbuk-dev <dybbuk555@gmail.com>
2023-06-07 06:39:05 +0200

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { AdvertisersModule } from './advertisers/advertisers.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.MYSQL_HOST,
-      port: process.env.MYSQL_PORT,
+      port: Number(process.env.MYSQL_PORT),
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DB,
